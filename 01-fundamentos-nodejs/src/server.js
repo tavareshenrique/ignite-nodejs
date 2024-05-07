d1c4b39c3c77e6b8ddd423a93a7bd32dbebd6771
Henrique Tavares <ihenrits@gmail.com>
2024-05-07 06:29:11 -0300

    refactor: work with routes

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de Usuários');
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de Usuário');
+  }
+
   return res.end('Hello World');
 })
 
