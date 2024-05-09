9b51d04c599f415214728c100a6e7954e1f5943a
Henrique Tavares <ihenrits@gmail.com>
2024-05-09 06:43:52 -0300

    feat: Add InverseNumberStream and MultiplyByTenStream classes for transforming and multiplying numbers in the stream

--- 01-fundamentos-nodejs/streams/fundamentals.js
+++ 01-fundamentos-nodejs/streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform, Duplex } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -18,4 +18,23 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const number = Number(chunk.toString())
+    const result = number * -1
+
+    callback(null, Buffer.from(`${result}\n`))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
