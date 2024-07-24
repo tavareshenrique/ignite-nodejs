5daf46d9b90751b4f4610904f630f75829b56459
Henrique Tavares <ihenrits@gmail.com>
2024-07-24 06:41:50 -0300

    feat: Add session ID to transaction creation
    This commit adds a session ID to the creation of transactions in the `/transactions` endpoint. If a session ID is not provided in the request cookies, a new session ID is generated and set as a cookie. The session ID is then included in the inserted transaction record.
    
    ```

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -49,11 +49,23 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions')
       .insert({
         id: randomUUID(),
         title,
         amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId,
       })
       .returning('*')
 
