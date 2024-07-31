a96c31e84555c009256cc45620f956fec64d1c5e
Henrique Tavares <ihenrits@gmail.com>
2024-07-31 06:29:54 -0300

    chore: Add .env.test.example and update .gitignore

--- 02-api-rest-nodejs/test/transactions.spec.ts
+++ 02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,5 +1,7 @@
+import { execSync } from 'node:child_process'
+
 import request from 'supertest'
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 
 import { app } from '../src/app'
 
@@ -10,6 +12,12 @@ describe('Transactions Routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
