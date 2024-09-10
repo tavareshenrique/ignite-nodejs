a8e74696667b52222eab14444cf25b70e972a4b9
Henrique Tavares <ihenrits@gmail.com>
2024-09-10 06:25:08 -0300

    feat: Handle UserAlreadyExistsError in register controller

--- 03-api-solid/src/use-cases/register.ts
+++ 03-api-solid/src/use-cases/register.ts
@@ -2,6 +2,8 @@ import { hash } from 'bcryptjs'
 
 import { IUserRepository } from '@/repositories/users-repository'
 
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
 interface IRegisterUseCaseRequest {
 	email: string
 	name: string
@@ -18,7 +20,7 @@ export class RegisterUseCase {
 		const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
 		if (userWithSameEmail) {
-			throw new Error('User with same email already exists')
+			throw new UserAlreadyExistsError()
 		}
 
 		await this.usersRepository.create({
