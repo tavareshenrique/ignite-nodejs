8bc23a1d8a357f8100b82e890e9ba17655603ddf
Henrique Tavares <ihenrits@gmail.com>
2024-09-16 08:06:44 -0300

    refactor: Add InMemoryUsersRepository implementation
    This commit adds the implementation of the InMemoryUsersRepository class, which is responsible for handling user data in memory. It includes methods for finding a user by email and creating a new user. This implementation is based on the IUsersRepository interface.
    
    Refactor the code to use the new repository implementation for user-related operations.

---
+++ 03-api-solid/src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker'
+import { Prisma, User } from '@prisma/client'
+
+import { IUsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements IUsersRepository {
+	public items: User[] = []
+
+	async findByEmail(email: string) {
+		const user = this.items.find((user) => user.email === email)
+
+		if (!user) {
+			return null
+		}
+
+		return user
+	}
+
+	async create(data: Prisma.UserCreateInput) {
+		const user = {
+			id: faker.string.uuid(),
+			name: data.name,
+			email: data.email,
+			password_hash: data.password_hash,
+			created_at: new Date(),
+		}
+
+		this.items.push(user)
+
+		return user
+	}
+}
