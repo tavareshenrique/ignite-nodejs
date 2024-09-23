dd2f14299d85118a0b16c27fea19cf13212e4e20
Henrique Tavares <ihenrits@gmail.com>
2024-09-23 07:26:12 -0300

    refactor: Update CheckInsRepository to prevent multiple check-ins on the same day

--- 03-api-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 03-api-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from 'node:crypto'
 
 import { CheckIn, Prisma } from '@prisma/client'
+import dayjs from 'dayjs'
 
 import { ICheckInsRepository } from '../check-ins-repository'
 
@@ -8,9 +9,16 @@ export class InMemoryCheckInsRepository implements ICheckInsRepository {
 	public items: CheckIn[] = []
 
 	async findByUserIdOnDate(userId: string, date: Date) {
-		const checkInOnSameDate = this.items.find(
-			(checkIn) => checkIn.user_id === userId,
-		)
+		const startOfTheDay = dayjs(date).startOf('date')
+		const endOfTheDay = dayjs(date).endOf('date')
+
+		const checkInOnSameDate = this.items.find((checkIn) => {
+			const checkInDate = dayjs(checkIn.created_at)
+			const isOnSameDate =
+				checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+			return checkIn.user_id === userId && isOnSameDate
+		})
 
 		if (!checkInOnSameDate) {
 			return null
