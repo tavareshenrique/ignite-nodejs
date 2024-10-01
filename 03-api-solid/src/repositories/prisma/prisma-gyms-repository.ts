c24af30763f9d3218dae93b0d0e95878ece6a88a
Henrique Tavares <ihenrits@gmail.com>
2024-10-01 07:57:17 -0300

    feat: Update PrismaGymsRepository and PrismaGymsRepository
    Refactor the PrismaGymsRepository to update the implementation and improve code quality.

---
+++ 03-api-solid/src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,56 @@
+import { Gym, Prisma } from '@prisma/client'
+
+import { prisma } from '@/lib/prisma'
+
+import { IFindManyNearbyParams, IGymsRepository } from '../gyms-repository'
+
+export class PrismaGymsRepository implements IGymsRepository {
+	async findById(id: string) {
+		const gym = await prisma.gym.findUnique({
+			where: {
+				id,
+			},
+		})
+
+		return gym
+	}
+
+	async findManyNearby({ latitude, longitude }: IFindManyNearbyParams) {
+		const gyms = await prisma.$queryRaw<Gym[]>`
+				SELECT
+					*
+				FROM
+					gyms
+				WHERE (
+						6371 *
+						acos( cos( radians(${latitude}) ) *
+						cos( radians( latitude ) ) *
+						cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) )
+				) <= 10
+		`
+
+		return gyms
+	}
+
+	async searchMany(query: string, page: number) {
+		const gyms = await prisma.gym.findMany({
+			where: {
+				name: {
+					contains: query,
+				},
+			},
+			take: 20,
+			skip: (page - 1) * 20,
+		})
+
+		return gyms
+	}
+
+	async create(data: Prisma.GymCreateInput) {
+		const gym = await prisma.gym.create({
+			data,
+		})
+
+		return gym
+	}
+}
