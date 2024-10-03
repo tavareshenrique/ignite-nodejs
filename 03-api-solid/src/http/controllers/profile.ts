8444ea5238425c5864e481abafdcb9728bca1b4a
Henrique Tavares <ihenrits@gmail.com>
2024-10-03 07:57:20 -0300

    refactor: Update profile controller to use getUserProfileUseCase
    - Replace direct JWT verification with getUserProfileUseCase to retrieve user profile
    - Remove console.log statement
    - Return user profile with password_hash field removed

--- 03-api-solid/src/http/controllers/profile.ts
+++ 03-api-solid/src/http/controllers/profile.ts
@@ -1,9 +1,18 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
+
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-	await request.jwtVerify()
+	const getUserProfile = makeGetUserProfileUseCase()
 
-	console.log('User:', request.user.sub)
+	const { user } = await getUserProfile.execute({
+		userId: request.user.sub,
+	})
 
-	return reply.status(200).send()
+	return reply.status(200).send({
+		user: {
+			...user,
+			password_hash: undefined,
+		},
+	})
 }
