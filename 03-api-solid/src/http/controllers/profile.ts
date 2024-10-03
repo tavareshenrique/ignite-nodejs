c5187b47a2338b12124dcae7e790f63b041045a6
Henrique Tavares <ihenrits@gmail.com>
2024-10-03 07:51:49 -0300

    feat: Add JWT token verification to profile controller

--- 03-api-solid/src/http/controllers/profile.ts
+++ 03-api-solid/src/http/controllers/profile.ts
@@ -1,22 +1,9 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-	// try {
-	// 	const authenticateUseCase = makeAuthenticateUseCase()
+	await request.jwtVerify()
 
-	// 	await authenticateUseCase.execute({
-	// 		email,
-	// 		password,
-	// 	})
-	// } catch (err) {
-	// 	if (err instanceof InvalidCredentialsError) {
-	// 		return reply.status(400).send({
-	// 			message: err.message,
-	// 		})
-	// 	}
-
-	// 	throw err
-	// }
+	console.log('User:', request.user.sub)
 
 	return reply.status(200).send()
 }
