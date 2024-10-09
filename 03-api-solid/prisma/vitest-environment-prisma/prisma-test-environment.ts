3041b9a7638ef200ea1120a151ae76d58bdcfd7a
Henrique Tavares <ihenrits@gmail.com>
2024-10-09 07:37:12 -0300

    chore: Update prisma-test-environment.ts to generate a unique schema for each test run and handle database setup and teardown

--- 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,41 @@
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
 import { Environment } from 'vitest/environments'
+
+import { prisma } from '@/lib/prisma'
+
+function generateDatabaseURL(schema: string) {
+	if (!process.env.DATABASE_URL) {
+		throw new Error('DATABASE_URL is not set')
+	}
+
+	const url = new URL(process.env.DATABASE_URL)
+
+	url.searchParams.set('schema', schema)
+
+	return url.toString()
+}
 
 export default <Environment>{
 	name: 'prisma',
 	transformMode: 'ssr',
 	async setup() {
-		console.log('Setup')
+		const schema = randomUUID()
+
+		process.env.DATABASE_URL = generateDatabaseURL(schema)
+
+		execSync('npx prisma migrate deploy')
 
 		return {
 			teardown: async () => {
-				console.log('Tear down')
+				await prisma.$executeRawUnsafe(
+					`DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+				)
+
+				await prisma.$disconnect()
 			},
 		}
 	},
