15dc03eec7beaf26689ce9cfc0485acb87a76409
Henrique Tavares <ihenrits@gmail.com>
2024-10-14 09:51:05 -0300

    test: Add e2e test for authentication

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -8,7 +8,7 @@ import { verifyJWT } from './middlewares/verify-jwt'
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/users', register)
 
-	app.post('/session', authenticate)
+	app.post('/sessions', authenticate)
 
 	/** Authenticated Bellow */
 	app.get('/me', { onRequest: [verifyJWT] }, profile)
