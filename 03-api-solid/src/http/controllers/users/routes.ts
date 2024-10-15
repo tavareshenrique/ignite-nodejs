85c8e4f4ad2ac0fb28227580de6aaed856fb36b2
Henrique Tavares <ihenrits@gmail.com>
2024-10-15 07:32:48 -0300

    refactor: Update app.ts to register gyms and users routes separately
    - The app.ts file was modified to register the gyms and users routes separately instead of using the appRoutes module.
    - This change improves the organization and maintainability of the codebase.

---
+++ 03-api-solid/src/http/controllers/users/routes.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { register } from './register'
+
+export async function userRoutes(app: FastifyInstance) {
+	app.post('/users', register)
+	app.post('/sessions', authenticate)
+
+	/** Authenticated Bellow */
+	app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
