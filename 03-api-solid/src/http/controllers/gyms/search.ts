a578d2aa7636c837e9383fd46bc5f87a293ac060
Henrique Tavares <ihenrits@gmail.com>
2024-10-15 07:38:01 -0300

    refactor: Update gyms routes to separate registration and nearby endpoints
    - Renamed the register function in the gyms create controller to create
    - Added a new nearby controller to handle nearby gym search
    - Updated the gyms routes file to include the new create and nearby endpoints
    - Created a new search controller to handle gym search functionality

---
+++ 03-api-solid/src/http/controllers/gyms/search.ts
@@ -0,0 +1,24 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+	const searchGymQuerySchema = z.object({
+		q: z.string(),
+		page: z.coerce.number().min(1).default(1),
+	})
+
+	const { page, q } = searchGymQuerySchema.parse(request.body)
+
+	const searchGymUseCase = makeSearchGymsUseCase()
+
+	const { gyms } = await searchGymUseCase.execute({
+		page,
+		query: q,
+	})
+
+	reply.status(200).send({
+		gyms,
+	})
+}
