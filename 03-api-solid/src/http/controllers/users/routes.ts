e703048f86c6305ae1a2f3c3b644375021fab2b1
Henrique Tavares <ihenrits@gmail.com>
2024-11-04 07:46:42 -0300

    feat: Implement refresh token endpoint and add end-to-end tests

--- 03-api-solid/src/http/controllers/users/routes.ts
+++ 03-api-solid/src/http/controllers/users/routes.ts
@@ -4,11 +4,14 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 import { register } from './register'
 
 export async function userRoutes(app: FastifyInstance) {
 	app.post('/users', register)
 	app.post('/sessions', authenticate)
+
+	app.patch('/token/refresh', refresh)
 
 	/** Authenticated Bellow */
 	app.get('/me', { onRequest: [verifyJWT] }, profile)
