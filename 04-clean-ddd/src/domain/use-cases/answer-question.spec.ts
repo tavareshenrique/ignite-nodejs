6f1a7c1b3e4fbd23d85660ad3cccc856986a58d2
Henrique Tavares <ihenrits@gmail.com>
2025-02-05 08:51:00 -0300

    feat: Add AnswersRepository interface and integrate it into AnswerQuestion use case

--- 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
+++ 04-clean-ddd/src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from "vitest"
 import { AnswerQuestion } from "./answer-question"
+import { AnswersRepository } from "../repositories/answers-repository"
+import { Answer } from "../entities/answer"
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestion()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return
+  }
+}
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestion(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     content: "Nova Resposta",
     instructorId: "123",
     questionId: "123"
--- 04-clean-ddd/src/domain/use-cases/answer-question.ts
+++ 04-clean-ddd/src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string;
@@ -7,12 +8,18 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestion {
-  execute({ content, instructorId, questionId }: AnswerQuestionUseCaseRequest) {
+  constructor(
+    private answersRepository: AnswersRepository
+  ) {}
+
+  async execute({ content, instructorId, questionId }: AnswerQuestionUseCaseRequest) {
     const answer = new Answer({
       content, 
       authorId: instructorId, 
       questionId
     });
+
+    await this.answersRepository.create(answer);
 
     return answer;
   }
