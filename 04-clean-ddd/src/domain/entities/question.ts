82a5f825a189c60a4234169b6d08b3d1e218aabc
Henrique Tavares <ihenrits@gmail.com>
2025-02-06 08:48:13 -0300

    refactor: Refactor Answer, Instructor, Question, and Student entities to extend base Entity class for consistent structure

--- 04-clean-ddd/src/domain/entities/question.ts
+++ 04-clean-ddd/src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from "node:crypto";
 import { Slug } from "./value-objects/slug";
+import { Entity } from "../../core/entities/entity";
 
 interface QuestionProps {
   title: string;
@@ -8,18 +8,6 @@ interface QuestionProps {
   authorId: string;
 }
 
-export class Question {
-  public id: string;
-  public title: string;
-  public content: string;
-  public slug: Slug;
-  public authorId: string;
-
-  constructor(props: QuestionProps, id?: string) {
-    this.id = id ?? randomUUID();
-    this.authorId = props.authorId;
-    this.title = props.title;
-    this.slug = props.slug;
-    this.content = props.content;
-  }
+export class Question extends Entity<QuestionProps> {
+ 
 }
