351927ea8e4fba92248d0cb70985be3996905505
Henrique Tavares <ihenrits@gmail.com>
2025-02-07 09:00:28 -0300

    refactor: Update import paths to use alias for core entities and types

--- 04-clean-ddd/src/domain/entities/question.ts
+++ 04-clean-ddd/src/domain/entities/question.ts
@@ -1,9 +1,9 @@
 import dayjs from "dayjs";
 
-import { Optional } from "../../core/types/optional";
+import { Optional } from "@/core/types/optional";
 
-import { Entity } from "../../core/entities/entity";
-import { UniqueEntityId } from "../../core/entities/unique-entity-id";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 
 import { Slug } from "./value-objects/slug";
 
