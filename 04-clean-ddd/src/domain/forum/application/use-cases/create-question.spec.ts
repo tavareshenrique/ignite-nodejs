b794485a44cef18888cf66d40b293e8a212cd39d
Henrique Tavares <ihenrits@gmail.com>
2025-02-24 09:22:03 -0300

    test: Add unit test for CreateQuestionUseCase

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,24 @@
+import { AnswerQuestion } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../../enterprise/entities/answer'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (_: Question) => {
+    return
+  },
+}
+
+test('create a question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'Nova Pergunta',
+    content: 'Nova Resposta',
+  })
+
+  expect(question.id).toBeTruthy()
+})
