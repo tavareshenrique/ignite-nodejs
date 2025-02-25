850ef152056ea1617dcf4d4bfa8fb4bfc4a0a15b
Henrique Tavares <ihenrits@gmail.com>
2025-02-25 09:47:27 -0300

    refactor: Replace direct Question instantiation with makeQuestion factory in GetQuestionBySlug tests

--- 04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,8 +1,7 @@
 import { InMemoryQuestionRepository } from 'test/repositories/in-memory-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { Question } from '../../enterprise/entities/question'
+import { makeQuestion } from 'test/factories/make-question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionRepository: InMemoryQuestionRepository
 let sut: GetQuestionBySlugUseCase
@@ -14,11 +13,8 @@ describe('Get Question By Slug', () => {
   })
 
   it('should be able to get a question by slug', async () => {
-    const newQuestion = Question.create({
-      title: 'Example Question',
-      content: 'Example Content',
+    const newQuestion = makeQuestion({
       slug: Slug.create('example-question'),
-      authorId: new UniqueEntityId(),
     })
 
     await inMemoryQuestionRepository.create(newQuestion)
