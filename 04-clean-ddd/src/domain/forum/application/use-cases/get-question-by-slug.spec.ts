44749fa513a1fbd274197b0d96649c8dd558caa7
Henrique Tavares <ihenrits@gmail.com>
2025-02-25 09:40:56 -0300

    test: Add unit tests for GetQuestionBySlug use case

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryQuestionRepository } from 'test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Question } from '../../enterprise/entities/question'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      title: 'Example Question',
+      content: 'Example Content',
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityId(),
+    })
+
+    await inMemoryQuestionRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toBe(newQuestion.title)
+
+  })
+})
