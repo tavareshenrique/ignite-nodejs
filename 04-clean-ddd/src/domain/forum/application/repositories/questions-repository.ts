99af75ac20aaf75b8d7baf4bb5ba751383d7c278
Henrique Tavares <ihenrits@gmail.com>
2025-02-26 09:12:54 -0300

    feat: Add save method to QuestionsRepository for persisting questions

--- 04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
+++ 04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
@@ -4,5 +4,6 @@ export interface QuestionsRepository {
   findById(questionId: string): Promise<Question | null>;
   findBySlug(slug: string): Promise<Question | null>;
   create(question: Question): Promise<void>;
+  save(question: Question): Promise<void>;
   delete(question: Question): Promise<void>;
 }
