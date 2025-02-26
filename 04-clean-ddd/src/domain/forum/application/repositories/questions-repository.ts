a0a4c4bc0f54c83770d81f04f3a87e283300571b
Henrique Tavares <ihenrits@gmail.com>
2025-02-26 08:59:55 -0300

    feat: Add findById and delete methods to QuestionsRepository interface

--- 04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
+++ 04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
@@ -1,6 +1,8 @@
 import { Question } from '../../enterprise/entities/question'
 
 export interface QuestionsRepository {
+  findById(questionId: string): Promise<Question | null>;
   findBySlug(slug: string): Promise<Question | null>;
   create(question: Question): Promise<void>;
+  delete(question: Question): Promise<void>;
 }
