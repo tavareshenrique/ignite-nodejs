e1919da72933126f4f497810322308efe5e2c203
Henrique Tavares <ihenrits@gmail.com>
2025-03-06 09:05:34 -0300

    feat: Add AnswerComment and QuestionComment entities for managing comments on answers and questions

---
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,52 @@
+import { Optional } from '@/core/types/optional'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export interface QuestionCommentProps {
+  authorId: UniqueEntityId
+  questionId: UniqueEntityId
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class QuestionComment extends Entity<QuestionCommentProps> {
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  static create(props: Optional<QuestionCommentProps, 'createdAt'>, id?: UniqueEntityId) {
+    const questionComment = new QuestionComment({
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    }, id)
+
+    return questionComment
+  }
+}
