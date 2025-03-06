891a7040052d0916d1070f353447e17a29e8bb81
Henrique Tavares <ihenrits@gmail.com>
2025-03-06 08:39:02 -0300

    fix: Allow custom createdAt date for Answer and Question entities

--- 04-clean-ddd/src/domain/forum/enterprise/entities/answer.ts
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/answer.ts
@@ -49,7 +49,7 @@ export class Answer extends Entity<AnswerProps> {
   static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId) {
     const answer = new Answer({
       ...props,
-      createdAt: new Date(),
+      createdAt: props.createdAt ?? new Date(),
     }, id)
 
     return answer
