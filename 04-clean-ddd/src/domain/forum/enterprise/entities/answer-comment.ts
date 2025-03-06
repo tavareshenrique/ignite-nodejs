c049bfdbaf1602d7f8d2613b4d33b46ccc10cd24
Henrique Tavares <ihenrits@gmail.com>
2025-03-06 09:43:59 -0300

    refactor: Extend AnswerComment and QuestionComment to inherit from Comment for shared properties

--- 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,44 +1,14 @@
 import { Optional } from '@/core/types/optional'
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Comment, CommentProps } from './comment'
 
-export interface AnswerCommentProps {
+export interface AnswerCommentProps extends CommentProps {
   authorId: UniqueEntityId
-  answerId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
-
-  get authorId() {
-    return this.props.authorId
-  }
-
+export class AnswerComment extends Comment<AnswerCommentProps> {
   get answerId() {
-    return this.props.answerId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+    return this.props.authorId
   }
 
   static create(props: Optional<AnswerCommentProps, 'createdAt'>, id?: UniqueEntityId) {
