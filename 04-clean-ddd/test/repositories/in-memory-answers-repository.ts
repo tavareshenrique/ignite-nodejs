509cb6557a03d6a92ca4012d27b209127bc5ff26
Henrique Tavares <ihenrits@gmail.com>
2025-03-06 08:53:04 -0300

    feat: Implement findManyByQuestionId method in InMemoryAnswersRepository for retrieving answers by question ID with pagination support

--- 04-clean-ddd/test/repositories/in-memory-answers-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-answers-repository.ts
@@ -1,8 +1,8 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
-export class InMemoryAnswerRepository implements AnswersRepository {
-
+export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
   async findById(answerId: string): Promise<Answer | null> {
@@ -13,6 +13,14 @@ export class InMemoryAnswerRepository implements AnswersRepository {
     }
 
     return answer
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async create(answer: Answer): Promise<void> {
