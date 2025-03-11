74d6dde2fa8dec85eef8874886ed27287eaf0933
Henrique Tavares <ihenrits@gmail.com>
2025-03-11 08:58:46 -0300

    feat: Implement findById and delete methods in InMemoryQuestionCommentsRepository for improved comment management

--- 04-clean-ddd/test/repositories/in-memory-question-comments-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-question-comments-repository.ts
@@ -4,7 +4,27 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
   public items: QuestionComment[] = []
 
+  async findById(questionId: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => item.id.toString() === questionId)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const questionCommentIndex = this.items.findIndex((item) => item.id === questionComment.id)
+
+    if (questionCommentIndex === -1) {
+      throw new Error('Question Comment not found')
+    }
+
+    this.items.splice(questionCommentIndex, 1)
   }
 }
