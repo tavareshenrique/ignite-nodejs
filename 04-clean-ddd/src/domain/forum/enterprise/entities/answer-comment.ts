de850eb10894cb81d1c6297d1de5db46930a9f65
Henrique Tavares <ihenrits@gmail.com>
2025-03-11 09:23:47 -0300

    fix: Correct answerId retrieval in AnswerComment class and update create method formatting

--- 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,22 +1,27 @@
 import { Optional } from '@/core/types/optional'
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Comment, CommentProps } from './comment'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 export interface AnswerCommentProps extends CommentProps {
-  authorId: UniqueEntityId
   answerId: UniqueEntityId
 }
 
 export class AnswerComment extends Comment<AnswerCommentProps> {
   get answerId() {
-    return this.props.authorId
+    return this.props.answerId
   }
 
-  static create(props: Optional<AnswerCommentProps, 'createdAt'>, id?: UniqueEntityId) {
-    const answerComment = new AnswerComment({
-      ...props,
-      createdAt: props.createdAt ?? new Date(),
-    }, id)
+  static create(
+    props: Optional<AnswerCommentProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const answerComment = new AnswerComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
 
     return answerComment
   }
