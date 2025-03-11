7399794e5fc96734705152202afaf1f8119c3980
Henrique Tavares <ihenrits@gmail.com>
2025-03-11 09:23:43 -0300

    feat: Implement findManyByAnswerId method in InMemoryAnswerCommentsRepository for paginated comment retrieval

--- 04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
@@ -13,6 +14,14 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
     }
 
     return answerComment
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
   }
 
   async create(answerComment: AnswerComment) {
