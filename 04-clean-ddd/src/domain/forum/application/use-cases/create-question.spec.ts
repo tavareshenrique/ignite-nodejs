1e7a9b84fa735038b748ab163f73744cf738728b
Henrique Tavares <ihenrits@gmail.com>
2025-03-20 09:04:43 -0300

    test: Update CreateQuestionUseCase tests to validate attachments handling

--- 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from './create-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
@@ -15,9 +16,15 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'Nova Pergunta',
       content: 'Nova Resposta',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBeTruthy()
-    expect(inMemoryQuestionsRepository.items[0]).toBe(result.value?.question)
+    expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
+    ])
   })
 })
