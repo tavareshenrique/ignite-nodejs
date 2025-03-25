066ff37de41650438c471036148e36014e16d563
Henrique Tavares <ihenrits@gmail.com>
2025-03-25 09:20:20 -0300

    feat: Integrate InMemoryQuestionAttachmentsRepository into CreateQuestionUseCase tests

--- 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,13 +1,16 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from './create-question'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
 
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
 
 describe('Create Question', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionAttachmentsRepository = new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(inMemoryQuestionAttachmentsRepository)
     sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
