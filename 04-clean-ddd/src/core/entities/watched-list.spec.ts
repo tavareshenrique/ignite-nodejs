a923a3a534af21e9262a13138d17c05edc1da99d
Henrique Tavares <ihenrits@gmail.com>
2025-03-25 08:48:28 -0300

    feat: Implement WatchedList class with add, remove, and update functionalities

---
+++ 04-clean-ddd/src/core/entities/watched-list.spec.ts
@@ -0,0 +1,69 @@
+import { WatchedList } from './watched-list'
+
+class NumberWatchedList extends WatchedList<number> {
+  compareItems(a: number, b: number): boolean {
+    return a === b
+  }
+}
+
+describe('watched list', () => {
+  it('should be able to create a watched list with initial items', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    expect(list.getItems()).toEqual([1, 2, 3])
+    expect(list.getItems()).toHaveLength(3)
+  })
+
+  it('should be able to add new items', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.add(4)
+
+    expect(list.getItems()).toEqual([1, 2, 3, 4])
+    expect(list.getItems()).toHaveLength(4)
+    expect(list.getNewItems()).toEqual([4])
+  })
+
+  it('should be able to remove items', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.remove(2)
+
+    expect(list.getItems()).toEqual([1, 3])
+    expect(list.getItems()).toHaveLength(2)
+    expect(list.getRemovedItems()).toEqual([2])
+  })
+
+  it('should be to add an item even if it was removed before', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.remove(2)
+    list.add(2)
+
+    expect(list.getItems()).toEqual([1, 3, 2])
+    expect(list.getItems()).toHaveLength(3)
+    expect(list.getRemovedItems()).toEqual([])
+    expect(list.getNewItems()).toEqual([])
+  })
+
+  it('should be to remove an item even if it was added before', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.add(4)
+    list.remove(4)
+
+    expect(list.getItems()).toEqual([1, 2, 3])
+    expect(list.getItems()).toHaveLength(3)
+    expect(list.getRemovedItems()).toEqual([])
+    expect(list.getNewItems()).toEqual([])
+  })
+
+  it('should be to update watched list items', () => {
+    const list = new NumberWatchedList([1, 2, 3])
+
+    list.update([1, 3, 5])
+
+    expect(list.getRemovedItems()).toEqual([2])
+    expect(list.getNewItems()).toEqual([5])
+  })
+})
