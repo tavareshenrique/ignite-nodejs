aa6821a2004540eb5cecdc7876eb6adc03ee07ab
Henrique Tavares <ihenrits@gmail.com>
2025-03-26 16:35:44 -0300

    test: Integrate InMemoryQuestionAttachmentsRepository into various use case tests

--- 04-clean-ddd/src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
+++ 04-clean-ddd/src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -5,15 +5,18 @@ import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questio
 import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
 import { makeQuestion } from 'test/factories/make-question'
 import { NotAllowedError } from './errors/not-allowed-error'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let sut: ChooseQuestionBestAnswerUseCase
 
 describe('Choose Question Best Answer', () => {
   beforeEach(() => {
     inMemoryAnswersRepository = new InMemoryAnswersRepository()
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionAttachmentsRepository = new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(inMemoryQuestionAttachmentsRepository)
     sut = new ChooseQuestionBestAnswerUseCase(
       inMemoryQuestionsRepository, inMemoryAnswersRepository,
     )
