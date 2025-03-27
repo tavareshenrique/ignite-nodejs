aee7fcc0af9879e7ef5bfef076f7fdc838cee8b1
Henrique Tavares <ihenrits@gmail.com>
2025-03-27 09:07:11 -0300

    test: Add unit tests for SendNotificationUseCase

---
+++ 04-clean-ddd/src/domain/notification/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,23 @@
+import { InMemoryNotificationRepository } from 'test/repositories/in-memory-notifications-repository'
+import { SendNotificationUseCase } from './send-notification'
+
+let inMemoryNotificationRepository: InMemoryNotificationRepository
+let sut: SendNotificationUseCase
+
+describe('Send Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationRepository = new InMemoryNotificationRepository()
+    sut = new SendNotificationUseCase(inMemoryNotificationRepository)
+  })
+
+  it('should be able to send a notification', async () => {
+    const result = await sut.execute({
+      recipientId: '1',
+      title: 'Nova notificação',
+      content: 'Conteúdo da notificação',
+    })
+
+    expect(result.isRight()).toBeTruthy()
+    expect(inMemoryNotificationRepository.items[0]).toEqual(result.value?.notification)
+  })
+})
