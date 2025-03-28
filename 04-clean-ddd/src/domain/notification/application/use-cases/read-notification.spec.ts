f47ec1a65ffb2f5d0a67585575f8d925843a98c8
Henrique Tavares <ihenrits@gmail.com>
2025-03-28 08:47:05 -0300

    test: Add unit tests for ReadNotificationUseCase

---
+++ 04-clean-ddd/src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,47 @@
+import { InMemoryNotificationRepository } from 'test/repositories/in-memory-notifications-repository'
+import { ReadNotificationUseCase } from './read-notification'
+import { makeNotification } from 'test/factories/make-notification'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/not-allowed-error'
+
+let inMemoryNotificationRepository: InMemoryNotificationRepository
+let sut: ReadNotificationUseCase
+
+describe('Read Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationRepository = new InMemoryNotificationRepository()
+    sut = new ReadNotificationUseCase(inMemoryNotificationRepository)
+  })
+
+  it('should be able to read a notification', async () => {
+    const notification = makeNotification()
+
+    await inMemoryNotificationRepository.create(notification)
+
+    const result = await sut.execute({
+      notificationId: notification.id.toString(),
+      recipientId: notification.recipientId.toString(),
+    })
+
+    expect(result.isRight()).toBeTruthy()
+    expect(inMemoryNotificationRepository.items[0].readAt).toEqual(
+      expect.any(Date),
+    )
+  })
+
+  it('should not be able to read a notification from another user', async () => {
+    const newAnswer = makeNotification({
+      recipientId: new UniqueEntityId('author-1'),
+    })
+
+    await inMemoryNotificationRepository.create(newAnswer)
+
+    const result = await sut.execute({
+      notificationId: newAnswer.id.toString(),
+      recipientId: 'author-2',
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+})
