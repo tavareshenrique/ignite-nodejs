458b3a1d60b56f1b6dfe0f0d7c68a33fee9dffde
Henrique Tavares <ihenrits@gmail.com>
2025-03-28 08:46:52 -0300

    feat: Add findById and save methods to NotificationsRepository and implement in-memory repository

--- 04-clean-ddd/test/repositories/in-memory-notifications-repository.ts
+++ 04-clean-ddd/test/repositories/in-memory-notifications-repository.ts
@@ -4,8 +4,28 @@ import { Notification } from '@/domain/notification/enterprise/entities/notifica
 export class InMemoryNotificationRepository implements NotificationsRepository {
   public items: Notification[] = []
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.items.find((notification) => notification.id.toString() === notificationId)
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
   async create(notification: Notification) {
     this.items.push(notification)
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.items.findIndex((item) => item.id === notification.id)
+
+    if (notificationIndex === -1) {
+      throw new Error('Notification not found')
+    }
+
+    this.items[notificationIndex] = notification
   }
 
 }
