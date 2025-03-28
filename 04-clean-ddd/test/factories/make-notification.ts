b43f1f01a79c9a8e0fad6b98fdde65a5394206fd
Henrique Tavares <ihenrits@gmail.com>
2025-03-28 08:46:57 -0300

    feat: Add read method to Notification entity and create factory function for notifications

---
+++ 04-clean-ddd/test/factories/make-notification.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Notification, NotificationProps } from '@/domain/notification/enterprise/entities/notification'
+
+export function makeNotification(
+  override: Partial<NotificationProps> = {},
+  id?: UniqueEntityId,
+) {
+  const notification = Notification.create({
+    title: faker.lorem.sentence(4),
+    content: faker.lorem.sentence(10),
+    recipientId: new UniqueEntityId(),
+    ...override,
+  }, id)
+
+  return notification
+}
