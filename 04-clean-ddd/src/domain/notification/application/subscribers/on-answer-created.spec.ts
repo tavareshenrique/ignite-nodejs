c9c9968553d5959dd7ec7d5ffd7e1c6a793a1c7c
Henrique Tavares <ihenrits@gmail.com>
2025-03-31 09:05:37 -0300

    test: Add unit tests for OnAnswerCreatedSubscriber to verify notification on answer creation

---
+++ 04-clean-ddd/src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -0,0 +1,22 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { OnAnswerCreated } from './on-answer-created'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+
+describe('OnAnswerCreatedSubscriber', () => {
+  beforeEach(() => {
+    inMemoryAnswerAttachmentsRepository = new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(inMemoryAnswerAttachmentsRepository)
+  })
+
+  it('should send a notification when an answer is created', () => {
+    new OnAnswerCreated()
+
+    const answer = makeAnswer()
+
+    inMemoryAnswersRepository.create(answer)
+  })
+})
