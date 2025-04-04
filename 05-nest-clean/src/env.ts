acbf293a0b1ff0b82e39e214c573aa0f405972d7
Henrique Tavares <ihenrits@gmail.com>
2025-04-04 09:31:13 -0300

    feat: replace JWT_SECRET with JWT_PRIVATE_KEY and JWT_PUBLIC_KEY in envSchema

--- 05-nest-clean/src/env.ts
+++ 05-nest-clean/src/env.ts
@@ -3,7 +3,8 @@ import { z } from 'zod';
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3333),
-  JWT_SECRET: z.string().min(1),
+  JWT_PRIVATE_KEY: z.string().min(1),
+  JWT_PUBLIC_KEY: z.string().min(1),
 });
 
 export type Env = z.infer<typeof envSchema>;
