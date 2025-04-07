b522d81d648a510803c1e7830e0221ec52ca128c
Henrique Tavares <ihenrits@gmail.com>
2025-04-07 09:22:58 -0300

    feat: enhance CreateQuestionController with body validation and slug generation

--- 05-nest-clean/src/controllers/create-question.controller.ts
+++ 05-nest-clean/src/controllers/create-question.controller.ts
@@ -1,17 +1,71 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CurrentUser } from 'src/auth/current-user-decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UserPayload } from 'src/auth/jwt.strategy';
+import { ZodValidationPipe } from 'src/pipes/zod-validation';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { z } from 'zod';
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
-  handle(@CurrentUser() user: UserPayload) {
-    console.log(user);
+  @HttpCode(201)
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    console.log('Creating question', body, user);
 
-    return 'ok';
+    const { title, content } = body;
+    const userId = user.sub;
+
+    const slug = this.convertToSlug(title);
+
+    const existingQuestion = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (existingQuestion) {
+      throw new ConflictException('Question with this slug already exists');
+    }
+
+    await this.prisma.question.create({
+      data: {
+        title,
+        content,
+        slug,
+        authorId: userId,
+      },
+    });
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-');
   }
 }
