964103db154d812d6ec3831c58658aafe049b5c1
Henrique Tavares <ihenrits@gmail.com>
2025-04-08 08:45:59 -0300

    feat: implement FetchRecentQuestionsController with pagination and JWT protection

--- 05-nest-clean/src/app.module.ts
+++ 05-nest-clean/src/app.module.ts
@@ -10,6 +10,7 @@ import { AuthModule } from './auth/auth.module';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { AuthenticateController } from './controllers/authenticate.controller';
 import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { CreateQuestionController } from './controllers/create-question.controll
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
