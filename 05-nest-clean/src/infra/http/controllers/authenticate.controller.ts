c9045a0679fcc16f0f22ac884c8083320e5abe0b
Henrique Tavares <ihenrits@gmail.com>
2025-04-24 08:50:21 -0300

    refactor: Simplify AuthenticateController by removing unused dependencies and integrating AuthenticateStudentUseCase

--- 05-nest-clean/src/infra/http/controllers/authenticate.controller.ts
+++ 05-nest-clean/src/infra/http/controllers/authenticate.controller.ts
@@ -1,17 +1,8 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  Post,
-  UnauthorizedException,
-  UsePipes,
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcryptjs';
+import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { z } from 'zod';
 
 import { ZodValidationPipe } from '../pipes/zod-validation';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
 
 const authenticateBodySchema = z.object({
   email: z.string().email(),
@@ -22,10 +13,7 @@ type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(
-    private prisma: PrismaService,
-    private jwt: JwtService,
-  ) {}
+  constructor(private authenticateStudent: AuthenticateStudentUseCase) {}
 
   @Post()
   @UsePipes(new ZodValidationPipe(authenticateBodySchema))
@@ -33,25 +21,16 @@ export class AuthenticateController {
   async handle(@Body() body: AuthenticateBodySchema) {
     const { email, password } = body;
 
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
+    const result = await this.authenticateStudent.execute({
+      email,
+      password,
     });
 
-    if (!user) {
-      throw new UnauthorizedException('Invalid credentials');
+    if (result.isLeft()) {
+      throw new Error('Internal server error');
     }
 
-    const isPasswordValid = await compare(password, user.password);
-
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
-
-    const accessToken = this.jwt.sign({
-      sub: user.id,
-    });
+    const { accessToken } = result.value;
 
     return {
       access_token: accessToken,
