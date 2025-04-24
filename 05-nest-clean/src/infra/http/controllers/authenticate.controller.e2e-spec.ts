c17907df9378d700bdd2b8d4470c4d195c7dcb2b
Henrique Tavares <ihenrits@gmail.com>
2025-04-24 08:56:50 -0300

    refactor: Update import paths for PrismaService to reflect new directory structure

--- 05-nest-clean/src/infra/http/controllers/authenticate.controller.e2e-spec.ts
+++ 05-nest-clean/src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { AppModule } from '@/infra/app.module';
-import { PrismaService } from '@/infra/prisma/prisma.service';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { hash } from 'bcryptjs';
