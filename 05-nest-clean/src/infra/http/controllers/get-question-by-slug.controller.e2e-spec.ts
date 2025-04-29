ed1b7276b4129325eef5353cd22cfc161d66a0c5
Henrique Tavares <ihenrits@gmail.com>
2025-04-29 09:16:35 -0300

    feat: Enhance GetQuestionBySlugController E2E tests with factories for student and question creation

--- 05-nest-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ 05-nest-clean/src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,23 +1,34 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
 import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/prisma/database.module';
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
+import { QuestionFactory } from 'test/factories/make-question';
+import { StudentFactory } from 'test/factories/make-student';
 
 describe('Get question by slug (E2E)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let studentFactory: StudentFactory;
+  let questionFactory: QuestionFactory;
   let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
 
     prisma = moduleRef.get(PrismaService);
+
+    studentFactory = moduleRef.get(StudentFactory);
+
+    questionFactory = moduleRef.get(QuestionFactory);
 
     jwt = moduleRef.get(JwtService);
 
@@ -25,25 +36,16 @@ describe('Get question by slug (E2E)', () => {
   });
 
   test('[GET] /questions/:slug', async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      },
-    });
+    const user = await studentFactory.makePrismaStudent();
 
     const accessToken = jwt.sign({
-      sub: user.id,
+      sub: user.id.toString(),
     });
 
-    await prisma.question.create({
-      data: {
-        title: 'How to create a NestJS application?',
-        slug: 'how-to-create-a-nestjs-application',
-        content: 'I want to create a NestJS application. How can I do that?',
-        authorId: user.id,
-      },
+    await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      title: 'How to create a NestJS application?',
+      slug: Slug.create('how-to-create-a-nestjs-application'),
     });
 
     const response = await request(app.getHttpServer())
