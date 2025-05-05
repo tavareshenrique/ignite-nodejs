9d7ea5b0030c425e0bec7b6d94ea438e187a8001
Henrique Tavares <ihenrits@gmail.com>
2025-05-05 09:12:59 -0300

    feat: Register DeleteQuestionCommentController and UseCase in HttpModule

--- 05-nest-clean/src/infra/http/http.module.ts
+++ 05-nest-clean/src/infra/http/http.module.ts
@@ -13,6 +13,7 @@ import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete
 import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers';
 import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer';
 import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question';
+import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment';
 
 import { CreateAccountController } from './controllers/create-account.controller';
 import { EditQuestionController } from './controllers/edit-question.controller';
@@ -27,6 +28,7 @@ import { DeleteAnswerController } from './controllers/delete-answer.controller';
 import { FetchQuestionAnswersController } from './controllers/fetch-questions.controller';
 import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller';
 import { CommentOnQuestionController } from './controllers/comment-on-question.controller';
+import { DeleteQuestionCommentController } from './controllers/delete-question-comment';
 
 import { CryptographyModule } from '../cryptography/cryptography.module';
 import { DatabaseModule } from '../database/prisma/database.module';
@@ -47,6 +49,7 @@ import { DatabaseModule } from '../database/prisma/database.module';
     FetchQuestionAnswersController,
     ChooseQuestionBestAnswerController,
     CommentOnQuestionController,
+    DeleteQuestionCommentController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -62,6 +65,7 @@ import { DatabaseModule } from '../database/prisma/database.module';
     FetchQuestionAnswersUseCase,
     ChooseQuestionBestAnswerUseCase,
     CommentOnQuestionUseCase,
+    DeleteQuestionCommentUseCase,
   ],
 })
 export class HttpModule {}
