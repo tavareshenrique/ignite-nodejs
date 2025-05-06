8fd56a15e84ba1a7c9321f5d859a07c7711773c0
Henrique Tavares <ihenrits@gmail.com>
2025-05-06 08:45:08 -0300

    fix: Update test description for deleting question comments

--- 05-nest-clean/src/infra/http/controllers/delete-question-comment.e2e-spec.ts
+++ 05-nest-clean/src/infra/http/controllers/delete-question-comment.e2e-spec.ts
@@ -9,7 +9,7 @@ import { QuestionFactory } from 'test/factories/make-question';
 import { QuestionCommentFactory } from 'test/factories/make-question-comment';
 import { StudentFactory } from 'test/factories/make-student';
 
-describe('Delete answer (E2E)', () => {
+describe('Delete question comment (E2E)', () => {
   let app: INestApplication;
   let studentFactory: StudentFactory;
   let questionFactory: QuestionFactory;
