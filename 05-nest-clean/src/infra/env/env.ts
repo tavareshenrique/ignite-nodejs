ff954e82104244f68c8ed3b8e6f2b00deba185a2
Henrique Tavares <ihenrits@gmail.com>
2025-05-12 10:50:53 -0300

    feat: add AWS environment variables to envSchema

--- 05-nest-clean/src/infra/env/env.ts
+++ 05-nest-clean/src/infra/env/env.ts
@@ -5,6 +5,9 @@ export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
   JWT_PRIVATE_KEY: z.string().min(1),
   JWT_PUBLIC_KEY: z.string().min(1),
+  AWS_BUCKET_NAME: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
