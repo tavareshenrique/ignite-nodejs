a59bfb8fd63756367b3367e6086b3fda9d028d71
Henrique Tavares <ihenrits@gmail.com>
2025-05-21 10:27:37 -0300

    feat: update CreateQuestionController to include attachments in request handling

--- 05-nest-clean/src/infra/http/controllers/create-question.controller.ts
+++ 05-nest-clean/src/infra/http/controllers/create-question.controller.ts
@@ -15,6 +15,7 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
 const createQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
+  attachments: z.array(z.string().uuid()).optional(),
 });
 
 const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
@@ -31,14 +32,14 @@ export class CreateQuestionController {
     @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
     @CurrentUser() user: UserPayload,
   ) {
-    const { title, content } = body;
+    const { title, content, attachments } = body;
     const userId = user.sub;
 
     const result = await this.createQuestion.execute({
       title,
       content,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments ?? [],
     });
 
     if (result.isLeft()) {
